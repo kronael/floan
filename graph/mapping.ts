f4211e8e86f31327a77a6992a072a3d0d2279721
Ondrej Vostal <ondrej@vostal.net>
2021-07-24 22:29:59 +0200

    Improve graph schema

--- graph/mapping.ts
+++ graph/mapping.ts
@@ -6,21 +6,27 @@ import {
 } from './generated/Floan/Floan'
 import {
   Loan,
-  Provider,
   RequestLoan,
   ProvideLoan,
-  DrawLoan,
   PaybackLoan,
+  DrawLoan,
 } from './generated/schema'
 
 export function handleLogRequestLoan(event: LogRequestLoan): void {
-  let loan = new Loan(event.params.loanID.toString())
+  let id = event.params.loanID.toString()
+  let ev = new RequestLoan(id)
 
-  loan.requester = event.params.requester
-  loan.principal = event.params.principal
-  loan.repayment = event.params.repayment
-  loan.duration = event.params.duration
-  loan.validUntil = event.params.validUntil
+  ev.requester = event.params.requester
+  ev.principal = event.params.principal
+  ev.repayment = event.params.repayment
+  ev.duration = event.params.duration
+  ev.validUntil = event.params.validUntil
+
+  ev.save()
+
+  let loan = new Loan(id)
+
+  loan.request = id
   loan.state = 'OPEN'
 
   loan.save()
@@ -29,25 +35,51 @@ export function handleLogRequestLoan(event: LogRequestLoan): void {
 export function handleLogProvideLoan(event: LogProvideLoan): void {
   let id = event.params.loanID.toString()
   let loan = Loan.load(id)
+  if(!loan)
+    return
+    
+  let ev = new ProvideLoan(id)
 
+  ev.loan = id
+  ev.provider = event.params.matcher
+
+  ev.save()
+
+  loan.provision = id
   loan.state = 'PROVIDED'
-  
+
   loan.save()
 }
 
 export function handleLogDrawLoan(event: LogDrawLoan): void {
   let id = event.params.loanID.toString()
   let loan = Loan.load(id)
+  if(!loan)
+    return
 
+  let ev = new DrawLoan(id)
+  ev.loan = id
+
+  ev.save()
+
+  loan.draw = id
   loan.state = 'DRAWN'
-  
+
   loan.save()
 }
 
 export function handleLogPaybackLoan(event: LogPaybackLoan): void {
   let id = event.params.loanID.toString()
   let loan = Loan.load(id)
+  if(!loan)
+    return
 
+  let ev = new PaybackLoan(id)
+  ev.loan = id
+
+  ev.save()
+
+  loan.repay = id
   loan.state = 'REPAYED'
 
   loan.save()
